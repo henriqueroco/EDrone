a38047c8deda6402e263d249a77e74715d980762
Henrique <henrique.roco@gmail.comk>
2015-10-29 21:52:43 -0200

    Configuração e entidade cliente

---
+++ EDrone/src/main/java/br/edrone/model/Cliente.java
@@ -0,0 +1,108 @@
+package br.edrone.model;
+
+import javax.persistence.Entity;
+import java.io.Serializable;
+import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Column;
+import javax.persistence.Version;
+@Entity
+@Table(name = "cliente")
+public class Cliente implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	@Column(name = "id", updatable = false, nullable = false)
+	private Long id;
+	@Version
+	@Column(name = "version")
+	private int version;
+
+	@Column(length = 80, name = "cliente_nome", nullable = false)
+	private String nome;
+
+	@Column(length = 200, name = "cliente_email", nullable = false)
+	private String email;
+
+	@Column(name = "cliente_senha", nullable = false)
+	private String senha;
+
+	public Long getId() {
+		return this.id;
+	}
+
+	public void setId(final Long id) {
+		this.id = id;
+	}
+
+	public int getVersion() {
+		return this.version;
+	}
+
+	public void setVersion(final int version) {
+		this.version = version;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!(obj instanceof Cliente)) {
+			return false;
+		}
+		Cliente other = (Cliente) obj;
+		if (id != null) {
+			if (!id.equals(other.id)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	@Override
+	public String toString() {
+		String result = getClass().getSimpleName() + " ";
+		if (nome != null && !nome.trim().isEmpty())
+			result += "nome: " + nome;
+		if (email != null && !email.trim().isEmpty())
+			result += ", email: " + email;
+		if (senha != null && !senha.trim().isEmpty())
+			result += ", senha: " + senha;
+		return result;
+	}
+}
