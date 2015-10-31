a942b3d6f925d3de0587998517707e775f9074fb
Henrique <henrique.roco@gmail.com>
2015-10-31 14:22:09 -0200

    Alteração entity cliente Criação Enum de sexo

--- EDrone/src/main/java/br/edrone/model/Cliente.java
+++ EDrone/src/main/java/br/edrone/model/Cliente.java
@@ -8,6 +8,11 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Column;
 import javax.persistence.Version;
+import java.util.Date;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import br.edrone.model.enuns.ESexo;
+import javax.persistence.Enumerated;
 @Entity
 @Table(name = "cliente")
 public class Cliente implements Serializable {
@@ -28,6 +33,27 @@ public class Cliente implements Serializable {
 
 	@Column(name = "cliente_senha", nullable = false)
 	private String senha;
+
+	@Column(length = 11, name = "cliente_rg")
+	private String rg;
+
+	@Column(length = 14, name = "cliente_cpf")
+	private String cpf;
+
+	@Column(name = "cliente_data_nacimento")
+	@Temporal(TemporalType.DATE)
+	private Date dataNacimento;
+
+	@Enumerated
+	@Column(name = "cliente_sexo")
+	private ESexo sexo;
+
+	@Column(name = "cliente_date_cadastro")
+	@Temporal(TemporalType.DATE)
+	private Date dataCadastro;
+
+	@Column(name = "cliente_telefone")
+	private String telefone;
 
 	public Long getId() {
 		return this.id;
@@ -94,6 +120,54 @@ public class Cliente implements Serializable {
 		this.senha = senha;
 	}
 
+	public String getRg() {
+		return rg;
+	}
+
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public Date getDataNacimento() {
+		return dataNacimento;
+	}
+
+	public void setDataNacimento(Date dataNacimento) {
+		this.dataNacimento = dataNacimento;
+	}
+
+	public ESexo getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(ESexo sexo) {
+		this.sexo = sexo;
+	}
+
+	public Date getDataCadastro() {
+		return dataCadastro;
+	}
+
+	public void setDataCadastro(Date dataCadastro) {
+		this.dataCadastro = dataCadastro;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
 	@Override
 	public String toString() {
 		String result = getClass().getSimpleName() + " ";
@@ -103,6 +177,12 @@ public class Cliente implements Serializable {
 			result += ", email: " + email;
 		if (senha != null && !senha.trim().isEmpty())
 			result += ", senha: " + senha;
+		if (rg != null && !rg.trim().isEmpty())
+			result += ", rg: " + rg;
+		if (cpf != null && !cpf.trim().isEmpty())
+			result += ", cpf: " + cpf;
+		if (telefone != null && !telefone.trim().isEmpty())
+			result += ", telefone: " + telefone;
 		return result;
 	}
 }
