eb44830430406fd15ada9d6df005a71901b8d17d
henriqueroco <henrique.roco@gmail.com>
2015-11-26 21:43:19 -0200

    Limpeza do projeto devido a problemas na criçäo dos relacionamentos

--- EDrone/src/main/webapp/scripts/services/CategoriaFactory.js
+++
@@ -1,4 +0,0 @@
-angular.module('eDrone').factory('CategoriaResource', function($resource){
-    var resource = $resource('rest/categoria/:CategoriaId',{CategoriaId:'@id'},{'queryAll':{method:'GET',isArray:true},'query':{method:'GET',isArray:false},'update':{method:'PUT'}});
-    return resource;
-});
