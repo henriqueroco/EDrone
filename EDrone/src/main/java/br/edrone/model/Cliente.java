ce6cf6d9b59f072a3a5d5f7bba2b9cc253821e4b
henriqueroco <henrique.roco@gmail.com>
2015-11-26 21:59:21 -0200

    Cria;áo de Models, Rest(endPoint), Dao, e o Carrinho

---
+++ EDrone/src/main/java/br/edrone/model/Cliente.java
@@ -0,0 +1,128 @@
+package br.edrone.model;
+
+import javax.persistence.Entity;
+import java.io.Serializable;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Column;
+import javax.persistence.Version;
+import java.lang.Override;
+import java.util.Set;
+import java.util.HashSet;
+import br.edrone.model.Venda;
+import javax.persistence.OneToMany;
+import javax.persistence.CascadeType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@Entity
+@XmlRootElement
+public class Cliente implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	@Column(name = "id", updatable = false, nullable = false)
+	private Long id;
+	@Version
+	@Column(name = "version")
+	private int version;
+
+	@OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL)
+	private Set<Venda> vendas = new HashSet<Venda>();
+
+	@Column(nullable = false)
+	private String nome;
+
+	@Column(nullable = false)
+	private String email;
+
+	@Column(nullable = false)
+	private String senha;
+
+	public Long getId() {
+		return this.id;
+	}
+
+	public void setId(final Long id) {
+		this.id = id;
+	}
+
+	public int getVersion() {
+		return this.version;
+	}
+
+	public void setVersion(final int version) {
+		this.version = version;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!(obj instanceof Cliente)) {
+			return false;
+		}
+		Cliente other = (Cliente) obj;
+		if (id != null) {
+			if (!id.equals(other.id)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	public Set<Venda> getVendas() {
+		return this.vendas;
+	}
+
+	public void setVendas(final Set<Venda> vendas) {
+		this.vendas = vendas;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	@Override
+	public String toString() {
+		String result = getClass().getSimpleName() + " ";
+		if (nome != null && !nome.trim().isEmpty())
+			result += "nome: " + nome;
+		if (email != null && !email.trim().isEmpty())
+			result += ", email: " + email;
+		if (senha != null && !senha.trim().isEmpty())
+			result += ", senha: " + senha;
+		return result;
+	}
+}
